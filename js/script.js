ce111dbf8e8ef0bafb1005c2fe094ba80449ecfb
Ka1ZenGuy <edgargexpositohdez@gmail.com>
2023-03-27 11:59:16 +0100

    botón no va

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 
 function generate(){
-    const nombres = ["Juan", "María", "Pedro", "Ana", "Luis", "Laura", "Diego", "Valeria", "Carlos", "Julieta"];
-    const apellidos = ["Pérez", "García", "Rodríguez", "González", "Martínez", "Hernández", "López", "Díaz", "Sánchez", "Romero"];
-    const rand_first = Math.floor(Math.random()* nombres.length);
-    const rand_last = Math.floor(Math.random()*lastname.length);
-    document.getElementById(`result`).innerHTML ="<h1>Nombre:</h1><div class='alert alert-success'><h2>"+nombres[rand_first]+" "+apellidos[rand_last]+"</h2></div>";
+    generate.addEventListener("click",()=> {
+
+        const nombres = ["Juan", "María", "Pedro", "Ana", "Luis", "Laura", "Diego", "Valeria", "Carlos", "Julieta"];
+        const apellidos = ["Pérez", "García", "Rodríguez", "González", "Martínez", "Hernández", "López", "Díaz", "Sánchez", "Romero"];
+        const rand_first = Math.floor(Math.random()* nombres.length);
+        const rand_last = Math.floor(Math.random()*lastname.length);
+        document.getElementById('result').innerHTML ="<h2>Nombre:</h2><div class='alert alert-success'><h4>"+nombres[rand_first]+" "+apellidos[rand_last]+"</h4></div>";
+    })
 
 }
